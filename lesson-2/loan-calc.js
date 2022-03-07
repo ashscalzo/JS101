679987e7adf87ed6b09edbc29ed0c68ea6e3a6bc
Ashlynn Scalzo <ash.scalzo@gmail.com>
2022-03-06 21:41:32 -0500

    added function to loan calc to test for invalid input values

--- lesson-2/loan-calc.js
+++ lesson-2/loan-calc.js
@@ -8,54 +8,89 @@ function dollarsAndCents(amount) {
   return amount.toFixed(2);
 }
 
-let loanAmt;
-let duration;
-let apr;
-let confirm;
+function invalidValue(value) {
+  return value.trimStart() === '' || Number.isNaN(Number(value));
+}
+
+let calcAgain;
 
 say('Hello! Welcome to LoanCalc!');
 
 do {
-  say('Please enter the original total amount of your loan:');
-  loanAmt = Number(readline.question());
 
-  say('Please enter the number of years your loan was originally to be paid out over:');
-  duration = Number(readline.question());
+  let loanAmt;
+  let duration;
+  let apr;
+  let confirm;
 
-  say('Is your loan an interest-bearing loan? (Enter y if yes.)');
-  apr = readline.question().toLowerCase();
-  if (apr === 'y') {
-    say('What is the annual interest rate (APR) on your loan?');
-    apr = Number(readline.question());
-  } else {
-    apr = null;
-  }
+  do {
+    say('Please enter the original total amount of your loan:');
+    loanAmt = readline.question();
 
-  say('Great! So, just to confirm:');
+    while (invalidValue(loanAmt)) {
+      say('Invalid value. Please enter the original total amount of your loan:');
+      loanAmt = readline.question();
+    }
+
+    loanAmt = Number(loanAmt);    //converted loanAmt to number
+
+    say('Please enter the original term of your loan in years:');
+    duration = readline.question();
+
+    while (invalidValue(duration)) {
+      say('Invalid value. Please enter the original term of your loan in years:');
+      duration = readline.question();
+    }
+
+    duration = Number(duration);    //converted duration to number
+
+    say('Is your loan an interest-bearing loan? (Enter y if yes, or any other character if no.)');
+    apr = readline.question().toLowerCase();
+
+    if (apr === 'y') {
+      say('What is the annual interest rate (APR) on your loan?');
+      apr = readline.question();
+
+      while (invalidValue(apr)) {
+        say('What is the annual interest rate (APR) on your loan?');
+        apr = readline.question();
+      }
+    } else {
+      apr = null;
+    }
+
+    say('Great! So, just to confirm:');
+    if (apr) {
+      say(`Your original loan was for $${loanAmt}, to be paid over ${duration} years at a ${apr}% annual rate?`);
+      say('Enter Y to confirm. Enter any other character to re-enter your information.');
+    } else {
+      say(`Your original loan was for $${loanAmt}, to be paid over ${duration} years with no interest?`);
+      say('Enter Y to confirm. Enter any other character to re-enter your information.');
+    }
+    confirm = readline.question().toLowerCase();
+  } while (confirm !== 'y');
+
+  let monthlyIntRate = (apr / 100) / 12;
+  let durationInMonths = duration * 12;
+  let monthlyPmt;
+
   if (apr) {
-    say(`Your original loan was for $${loanAmt}, to be paid over ${duration} years at a ${apr}% annual rate?`);
-    say('Enter Y to confirm. Enter any other character to re-enter your information.');
+    monthlyPmt = loanAmt *
+      (monthlyIntRate / (1 - ((1 + monthlyIntRate) ** (-durationInMonths))));
+    let totalInt = (monthlyPmt * durationInMonths) - loanAmt;
+    say(`Your installments for this loan will be $${dollarsAndCents(monthlyPmt)} per month.`);
+    say(`The total amount of interest paid on this loan will be $${dollarsAndCents(totalInt)}.`);
   } else {
-    say(`Your original loan was for $${loanAmt}, to be paid over ${duration} years with no interest?`);
-    say('Enter Y to confirm. Enter any other character to re-enter your information.');
+    monthlyPmt = loanAmt / durationInMonths;
+    say(`Your installments for this loan will be $${dollarsAndCents(monthlyPmt)} per month.`);
   }
-  confirm = readline.question().toLowerCase();
-} while (confirm !== 'y');
 
-let monthlyIntRate = (apr / 100) / 12;
-let durationInMonths = duration * 12;
-let monthlyPmt;
+  say('Would you like to take a look at another loan?');
+  say("Enter Y to calculate another loan's payments. Enter any other character to exit.");
+  calcAgain = readline.question().toLowerCase();
+} while (calcAgain === 'y');
 
-if (apr) {
-  monthlyPmt = loanAmt *
-    (monthlyIntRate / (1 - ((1 + monthlyIntRate) ** (-durationInMonths))));
-  let totalInt = (monthlyPmt * durationInMonths) - loanAmt;
-  say(`Your installments for this loan will be $${dollarsAndCents(monthlyPmt)} per month.`);
-  say(`The total amount of interest paid on this loan will be $${dollarsAndCents(totalInt)}.`);
-} else {
-  monthlyPmt = loanAmt / durationInMonths;
-  say(`Your installments for this loan will be $${dollarsAndCents(monthlyPmt)} per month.`);
-}
+say('Thank you for using LoanCalc!');
 
 /*
 [years, months]
