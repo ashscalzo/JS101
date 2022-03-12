363653726733ccf85ef3ad4888f792d16bbfc70a
Ashlynn Scalzo <ash.scalzo@gmail.com>
2022-03-12 14:15:24 -0500

    changed RPSLS to best 3 out of 5, add grand winner total and reset wins/losses to 0 on new game

--- lesson-2/rock-paper-scissors.js
+++ lesson-2/rock-paper-scissors.js
@@ -7,15 +7,15 @@ const WINNING_COMBOS = {
   lizard: ['spock', 'paper'],
   spock: ['rock', 'scissors']
 };
-
 let winsAndLosses = [0, 0];
+let grandWins = [0, 0];
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function playerChoice(input) {
-  switch (input) {
+function playerChoice(playInput) {
+  switch (playInput) {
     case 'rock':
     case 'r':
       return 'rock';
@@ -23,16 +23,16 @@ function playerChoice(input) {
     case 'p':
       return 'paper';
     case 'scissors':
-    case 's':
+    case 'sc':
       return 'scissors';
     case 'lizard':
     case 'l':
       return 'lizard';
     case 'spock':
-    case 'x':
+    case 'sp':
       return 'spock';
   }
-  return input;
+  return playInput;
 }
 
 function playerWins(player, comp) {
@@ -45,11 +45,11 @@ function computerWins(player, comp) {
 
 function displayWinner(player, comp) {
   if (playerWins(player, comp)) {
-    prompt('You win!');
+    prompt('You get one (1) point!');
   } else if (computerWins(player, comp)) {
-    prompt('Computer wins!');
+    prompt('Computer gains one (1) point!');
   } else {
-    prompt("It's a tie.");
+    prompt('Round tied.');
   }
 }
 
@@ -61,34 +61,62 @@ function countWinsAndLosses(player, comp) {
   }
 }
 
-while (true) {
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let playerInput = readline.question().toLowerCase();
-  let choice = playerChoice(playerInput);
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt("That's not a valid choice!");
-    choice = readline.question();
-  }
-
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
-
-  prompt(`You chose ${choice}, computer chose ${computerChoice}.`);
-
-  displayWinner(choice, computerChoice);
-  countWinsAndLosses(choice, computerChoice);
-
-  prompt('Do you want to play again? (y/n)');
+function newGame() {
+  prompt('Do you want to play again? (yes or no)');
   let answer = readline.question().toLowerCase();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
+  while (answer !== 'n' &&
+         answer !== 'no' &&
+         answer !== 'y' &&
+         answer !== 'yes') {
+    prompt('Invalid input. Do you want to play again? (yes or no)');
     answer = readline.question().toLowerCase();
   }
+  return answer;
+}
 
-  if (answer[0] !== 'y') break;
+while (true) {
+  prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!');
+  prompt('Winning choice receives 1 point per round.');
+  prompt('First to 3 points is the Grand Winner.');
+
+  do {
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')} (R, P, SC, L, or SP).`);
+    let input = readline.question().toLowerCase();
+    let choice = playerChoice(input);
+
+    while (!VALID_CHOICES.includes(choice)) {
+      prompt("That's not a valid choice!");
+      input = readline.question().toLowerCase();
+      choice = playerChoice(input);
+    }
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    prompt(`You chose ${choice}, computer chose ${computerChoice}.`);
+
+    displayWinner(choice, computerChoice);
+    countWinsAndLosses(choice, computerChoice);
+
+    if (winsAndLosses[0] === 3) {
+      prompt("You're the grand winner!");
+      grandWins[0] += 1;
+    } else if (winsAndLosses[1] === 3) {
+      prompt("The computer is the grand winner! Try again!");
+      grandWins[1] += 1;
+    }
+
+  } while (winsAndLosses[0] < 3 && winsAndLosses[1] < 3);
+
+  let playAgain = newGame();
+  if (playAgain === 'y' || playAgain === 'yes') {
+    winsAndLosses = [0, 0];
+  } else if (playAgain === 'n' || playAgain === 'no') {
+    break;
+  }
 }
 
 console.clear();
 prompt('Thanks for playing!');
-prompt(`Total wins: ${winsAndLosses[0]} / Total losses: ${winsAndLosses[1]}`);
+prompt(`You won ${grandWins[0]} game(s)!`);
+prompt(`Computer won ${grandWins[1]} game(s)!`);
