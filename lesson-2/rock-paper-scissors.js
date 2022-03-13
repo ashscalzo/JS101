e9aa2a186de526d9ab17d9cca21ec56a9c84c447
Ashlynn Scalzo <ash.scalzo@gmail.com>
2022-03-13 19:36:17 -0400

    changed some function names for better clarity

--- lesson-2/rock-paper-scissors.js
+++ lesson-2/rock-paper-scissors.js
@@ -1,5 +1,5 @@
 const readline = require('readline-sync');
-const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];      //let winsAndLosses = [0, 0];
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
 const WINNING_COMBOS = {
   rock: ['scissors', 'lizard'],
   paper: ['rock', 'spock'],
@@ -7,14 +7,14 @@ const WINNING_COMBOS = {
   lizard: ['spock', 'paper'],
   spock: ['rock', 'scissors']
 };
-let winsAndLosses = [0, 0];
-let grandWins = [0, 0];
+let winsAndLosses = [0, 0];   //calculates how many rounds won (index 0 is player, index 1 is comp)
+let grandWins = [0, 0];     // index 0 is # of times player wins, index 1 is # of times comp wins
 
 function prompt(message) {
   console.log(`=> ${message}`);
 }
 
-function playerChoice(playInput) {
+function convertPlayerChoice(playInput) { //converts player input to valid choice
   switch (playInput) {
     case 'rock':
     case 'r':
@@ -35,18 +35,18 @@ function playerChoice(playInput) {
   return playInput;
 }
 
-function playerWins(player, comp) {
+function definePlayerWins(player, comp) {
   return WINNING_COMBOS[player].includes(comp);
 }
 
-function computerWins(player, comp) {
+function defineComputerWins(player, comp) {
   return WINNING_COMBOS[comp].includes(player);
 }
 
 function displayWinner(player, comp) {
-  if (playerWins(player, comp)) {
+  if (definePlayerWins(player, comp)) {
     prompt('You get one (1) point!');
-  } else if (computerWins(player, comp)) {
+  } else if (defineComputerWins(player, comp)) {
     prompt('Computer gains one (1) point!');
   } else {
     prompt('Round tied.');
@@ -54,14 +54,14 @@ function displayWinner(player, comp) {
 }
 
 function countWinsAndLosses(player, comp) {
-  if (playerWins(player, comp)) {
+  if (definePlayerWins(player, comp)) {
     winsAndLosses[0] += 1;
-  } else if (computerWins(player, comp)) {
+  } else if (defineComputerWins(player, comp)) {
     winsAndLosses[1] += 1;
   }
 }
 
-function newGame() {
+function startNewGame() {
   prompt('Do you want to play again? (yes or no)');
   let answer = readline.question().toLowerCase();
   while (answer !== 'n' &&
@@ -80,14 +80,14 @@ while (true) {
   prompt('First to 3 points is the Grand Winner.');
 
   do {
-    prompt(`Choose one: ${VALID_CHOICES.join(', ')} (R, P, SC, L, or SP).`);
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')}. (or R, P, SC, L, or SP)`);
     let input = readline.question().toLowerCase();
-    let choice = playerChoice(input);
+    let choice = convertPlayerChoice(input);
 
     while (!VALID_CHOICES.includes(choice)) {
       prompt("That's not a valid choice!");
       input = readline.question().toLowerCase();
-      choice = playerChoice(input);
+      choice = convertPlayerChoice(input);
     }
 
     let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
@@ -98,19 +98,20 @@ while (true) {
     displayWinner(choice, computerChoice);
     countWinsAndLosses(choice, computerChoice);
 
-    if (winsAndLosses[0] === 3) {
-      prompt("You're the grand winner!");
-      grandWins[0] += 1;
-    } else if (winsAndLosses[1] === 3) {
-      prompt("The computer is the grand winner! Try again!");
-      grandWins[1] += 1;
-    }
-
   } while (winsAndLosses[0] < 3 && winsAndLosses[1] < 3);
 
-  let playAgain = newGame();
+  if (winsAndLosses[0] === 3) {
+    prompt("You're the grand winner!");
+    grandWins[0] += 1;
+  } else if (winsAndLosses[1] === 3) {
+    prompt("The computer is the grand winner! Try again!");
+    grandWins[1] += 1;
+  }
+
+  let playAgain = startNewGame();
   if (playAgain === 'y' || playAgain === 'yes') {
-    winsAndLosses = [0, 0];
+    winsAndLosses = [0, 0];       // reset score for new game
+    console.clear();
   } else if (playAgain === 'n' || playAgain === 'no') {
     break;
   }
