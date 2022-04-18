fcf2346097c9447d693db5c578b04966cf8f037d
Ashlynn Scalzo <ash.scalzo@gmail.com>
2022-04-18 19:20:26 -0400

    labeled test cases, removed extra line that was not needed

--- JS101-small-problems/medium-1/rotation-1.js
+++ JS101-small-problems/medium-1/rotation-1.js
@@ -17,6 +17,8 @@ function rotateArray(arr) {
   }
 }
 
+
+// test cases
 console.log(rotateArray([7, 3, 5, 2, 9, 1]));         // [3, 5, 2, 9, 1, 7]
 console.log(rotateArray(['a', 'b', 'c']));            // ["b", "c", "a"]
 console.log(rotateArray(['a']));                      // ["a"]
@@ -26,7 +28,6 @@ console.log(rotateArray([]));                         // []
 
 console.log(rotateArray());                         // undefined
 console.log(rotateArray(1));                        // undefined
-
 
 let array = [1, 2, 3, 4];
 console.log(rotateArray(array));                    // [2, 3, 4, 1]
