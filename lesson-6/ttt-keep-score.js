7e29f9f70ce8e29aedd3f08d486adcf898050018
ashscalzo <ash.scalzo@gmail.com>
2022-09-27 18:51:20 -0400

    modifications made to ttt-keep-score.js (made a while ago and some added to tic tac toe game

--- lesson-6/ttt-keep-score.js
+++ lesson-6/ttt-keep-score.js
@@ -1,24 +1,33 @@
 // Keep Score
-// Keep track of how many times the player and computer each win, and report the scores after each game. 
-// The first player to win 5 games wins the overall match (a series of 2 or more games). The score 
-// should reset to 0 for each player when beginning a new match. Don't use any global variables. 
-// However, you may want to use a global constant to represent the number of games needed to win the match.
+// Keep track of how many times the player and computer each
+// win, and report the scores after each game. The first player to win
+// 5 games wins the overall match (a series of 2 or more games). The score
+// should reset to 0 for each player when beginning a new match. Don't use
+// any global variables. However, you may want to use a global constant
+// to represent the number of games needed to win the match.
 
 const WINNING_POINTS = 3;
+let currentScore = [0, 0];   // index 0 is player points, index 1 is comp points
 
-
-function keepScore() {
-  let currentScore = [0, 0];      // index 0 is player points, index 1 is comp points
-  if (detectWinner(board) === 'Player') {
-    currentScore[0] += 1;
-  } else if (detectWinner(board) === 'Computer') {
-    currentScore[1] += 1;
+function countScore(scoreArr) {
+  let score = scoreArr;      // index 0 is player points, index 1 is comp points
+  while (score[0] < WINNING_POINTS && score[1] < WINNING_POINTS) {
+    if (detectWinner(board) === 'Player') {
+      score[0] += 1;
+    } else if (detectWinner(board) === 'Computer') {
+      score[1] += 1;
+    }
   }
+  return score;
 }
 
 function declareWinner() {
   if (currentScore[0] === 3) {
     prompt(`Player wins!`);
+    prompt(`Final Score:  
+      Player: ${currentScore[0]} / Computer: ${currentScore[1]}`);
+  } else if (currentScore[1] === 3) {
+    prompt(`Computer wins!`);
     prompt(`Final Score:  
       Player: ${currentScore[0]} / Computer: ${currentScore[1]}`);
   }
